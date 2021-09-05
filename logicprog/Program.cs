c5c09d7f7f1462590f8dd5349c5d2ad11d191e91
sonupooja <ingalahallipooja@gmail.com>
2021-09-05 22:29:55 +0530

    adding stopwatch program

---
+++ logicprog/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace logicprog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to logical programs");
+        }
+    }
+}
