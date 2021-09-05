559bf8b60e440ecedfa0621f8f5aa83f9c7818aa
sonupooja <ingalahallipooja@gmail.com>
2021-09-05 23:03:17 +0530

    checking coupon number

--- logicprog/Program.cs
+++ logicprog/Program.cs
@@ -1,21 +1,34 @@
 ﻿using System;
 
-namespace reverse
+namespace Coupon_Number
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int n, reverse = 0, rem;
-            Console.WriteLine("Enter a number: ");
-            n = int.Parse(Console.ReadLine());
-            while (n != 0)
-            {
-                rem = n % 10;
-                reverse = reverse * 10 + rem;
-                n /= 10;
-            }
-            Console.Write("Reversed Number: " + reverse);
-        }
-    }
+	class Program
+	{
+		public class Coupons
+		{
+			
+			public Coupons()
+			{
+				int distinct = 0, count = 0;
+				Console.WriteLine("Enter a number of given distinct coupons:");
+				int couponNo = Convert.ToInt32(Console.ReadLine());
+				bool[] isCollected = new bool[couponNo];
+				while (distinct < couponNo)
+				{
+					Random random = new Random();
+					int newCoupon = (int)(random.NextDouble() * couponNo);
+					count++;
+					if (!isCollected[newCoupon])
+					{
+						distinct++;
+						isCollected[newCoupon] = true;
+					}
+				}
+
+				Console.WriteLine("Total random number needed to have all distinct coupons:" + count);
+			}
+		}
+	}
 }
+
+
