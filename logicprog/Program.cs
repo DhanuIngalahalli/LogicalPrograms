2d2832e8fe480c4d81c140f0d8bdf9dcf3912af4
sonupooja <ingalahallipooja@gmail.com>
2021-09-05 22:40:33 +0530

    adding fibonacci number

--- logicprog/Program.cs
+++ logicprog/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 
-namespace logicprog
+namespace fibonacci
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to logical programs");
+            int num1 = 0, num2 = 1, num3, i, number;
+            Console.Write("Enter the number of elements: ");
+            number = int.Parse(Console.ReadLine());
+            Console.Write(num1 + " " + num2 + " "); //printing 0 and 1    
+            for (i = 2; i < number; ++i) //loop starts from 2 because 0 and 1 are already printed    
+            {
+                num3 = num1 + num2;
+                Console.Write(num3 + " ");
+                num1 = num2;
+                num2 = num3;
+            }
         }
     }
 }
